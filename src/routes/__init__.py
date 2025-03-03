df0f45d0f16cd3d73d9769684ff711cd17924ab4
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-02 23:10:39 -0300

    feat: include routes on main route

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter
 
+from src.routes import CardRouter, PingRouter, UserRouter
 
 main_router = APIRouter(prefix="/api")
+
+main_router.include_router(UserRouter.router)
+main_router.include_router(PingRouter.router)
+main_router.include_router(CardRouter.router)
