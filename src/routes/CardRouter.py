d57005457fe3bb650e244aaa1835ffb769570a6a
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-02 23:09:41 -0300

    feat: create card router

---
+++ src/routes/CardRouter.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from prisma.partials import CardWithoutRelation
+from src.dto.CreateCardDto import CreateCardDto
+from src.injector import Resolve
+from src.service.CardService import CardService
+
+router = APIRouter()
+
+card_service = Resolve(CardService)
+
+@router.post("/card", response_model=CardWithoutRelation) 
+async def create_card(create_card_dto: CreateCardDto):
+    return await card_service().create(create_card_dto.user_id, create_card_dto.title)
