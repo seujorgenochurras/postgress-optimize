07094eb8d73739d6ea24001d7d98cf102f56d7f9
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-02 23:20:01 -0300

    docs(swagger): add tag in user router

--- src/routes/UserRouter.py
+++ src/routes/UserRouter.py
@@ -3,16 +3,18 @@ from src.dto.UserDto import CreateUserDto
 from src.injector import Resolve
 from src.service.UserService import UserService
 from prisma.partials import UserSafe
-router = APIRouter()
+
+
+router = APIRouter(prefix="/user", tags=["user"])
 
 user_service = Resolve(UserService)
 
 
-@router.get("/user", response_model=list[UserSafe])
+@router.get("/", response_model=list[UserSafe])
 async def find_all_users():
     return await user_service().find_all()
 
 
-@router.post("/user")
+@router.post("/")
 async def create_user(user: CreateUserDto):
     return await user_service().create(user)
