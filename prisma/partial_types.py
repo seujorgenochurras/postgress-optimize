a22672e9f1b8c17e3ba7d2c79e72e3c14ffa545c
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-04 21:13:00 -0300

    feat: create simple task partial domain type

--- prisma/partial_types.py
+++ prisma/partial_types.py
@@ -1,5 +1,9 @@
-from prisma.models import Card, User
+from prisma.models import Card, User, Task
 
 Card.create_partial("CardWithoutRelation", exclude_relational_fields=True)
 
-User.create_partial("UserSafe", relations={"cards" : "CardWithoutRelation"})
+User.create_partial("UserSafe", relations={"cards": "CardWithoutRelation"})
+
+Task.create_partial(
+    "SimpleTask", exclude_relational_fields=True, exclude=["cardId", "id"]
+)
